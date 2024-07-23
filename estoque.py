5fb8137e553adf9416988b83bdcb16e2a8ce8c49
Iago Flávio <iagoflavio_sa@hotmail.com>
2024-07-23 18:48:09 -0300

    order_by_quantity() functional

--- estoque.py
+++ estoque.py
@@ -37,13 +37,33 @@ class Stock:
 
         ## Search in all of the products list and return
         for i in range(len(all_products)):
+            highest = None
+
             for product in all_products:
-                highest = product
-                
-                if product.quantidade >= highest.quantidade:
+                if highest == None:
                     highest = product
+                
+                if product.quantity >= highest.quantity:
+                    highest = product
+                    print(f"O produto atual é {product.quantity}")
+                    print(f"O highest {highest.quantity} foi atribuido como produto")
+
+            print(f"O highest que será feito o append e removido é: {highest.quantity}")
                 
             ordered_list.append(highest)
             all_products.remove(highest)
 
         return ordered_list
+    
+teste = Stock()
+
+teste.stock.add(Product(1, 'iago', 'legal', 1, 1))
+teste.stock.add(Product(2, 'hertz', 'interessante', 3, 2))
+teste.stock.add(Product(3, 'caua', 'opressor', 5, 7))
+teste.stock.add(Product(4, 'luana', 'opressor', 4, 9))
+
+
+for product in teste.stock.get_all():
+    print(product.name, product.quantity, sep=',')
+for product in teste.order_by_quantity():
+    print(product.name, product.quantity)
