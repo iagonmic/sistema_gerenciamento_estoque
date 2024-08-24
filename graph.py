e40ffb37a9e95723b32a33e553afe531fca5edfa
Iago Flávio <iagoflavio_sa@hotmail.com>
2024-08-24 20:44:18 -0300

    remove_node functiona added and fixed more typos, plus added default value on add_node function

--- graph.py
+++ graph.py
@@ -4,12 +4,12 @@ class Graph:
         self.structure = {}
 
     def get_nodes(self, origin):
-        if origin in self.structure:
+        if origin in self.structure.keys:
             return self.structure.get(origin)
         
         return []
     
-    def add_node(self, origin, end, ordered: bool):
+    def add_node(self, origin, end, ordered: bool = False):
         nodes = self.get_nodes(origin)
         nodes.append(end)
         
@@ -17,3 +17,17 @@ class Graph:
 
         if not ordered:
             self.add_node(end, origin, True)
+
+    def remove_node(self, origin, ordered: bool = False):
+        if origin in self.structure.keys:
+            origin_nodes = self.get_nodes(origin)  
+            self.structure.pop(origin)
+
+            if not ordered:
+                for node in origin_nodes:
+                    node_nodes = self.get_nodes(node)
+                    node_nodes.remove(origin)
+
+                    self.structure[node] = node_nodes
+
+            
