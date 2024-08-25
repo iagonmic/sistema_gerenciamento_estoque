a39cf62183d5c795286c64ca25942bfad51c8c49
Iago Flávio <iagoflavio_sa@hotmail.com>
2024-08-25 07:59:15 -0300

    Changed .keys method back and created contains_node function

--- graph.py
+++ graph.py
@@ -4,7 +4,7 @@ class Graph:
         self.structure = {}
 
     def get_nodes(self, origin):
-        if origin in self.structure.keys:
+        if origin in self.structure:
             return self.structure.get(origin)
         
         return []
@@ -18,16 +18,22 @@ class Graph:
         if not ordered:
             self.add_node(end, origin, True)
 
+    def contains_node(self, origin):
+        if origin in self.structure:
+            return True
+        return False
+
     def remove_node(self, origin, ordered: bool = False):
-        if origin in self.structure.keys:
+        if self.contains_node(origin):
             origin_nodes = self.get_nodes(origin)  
             self.structure.pop(origin)
 
             if not ordered:
                 for node in origin_nodes:
-                    node_nodes = self.get_nodes(node)
-                    node_nodes.remove(origin)
+                    if self.contains_node(node):
+                        node_nodes = self.get_nodes(node)
+                        node_nodes.remove(origin)
 
-                    self.structure[node] = node_nodes
+                        self.structure[node] = node_nodes
 
             
